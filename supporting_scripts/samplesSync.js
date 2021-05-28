88a5bb13d49834c51f3af77d2502237efc26f01f
brennan <brennanhfredericks@gmail.com>
2021-05-28 13:28:25 +0200

    adding validation schemas

--- supporting_scripts/samplesSync.js
+++ supporting_scripts/samplesSync.js
@@ -5,6 +5,86 @@
 // easier control flow
 const { readdirSync, fstatSync, openSync, closeSync, readFileSync } = require('fs');
 const Path = require('path');
+const Ajv = require("ajv");
+const { type } = require('os');
+const { compileSchema } = require('ajv/dist/compile');
+const ajv = new Ajv({ allErrors: true })
+
+
+// packet schema confirm top level
+const schemaPacket = {
+    type: 'object',
+    properties: {
+        Info: { type: 'object' },
+        AF_Packet: { type: 'object' }
+    },
+    required: ['Info', 'AF_Packet'],
+    additionalProperties: true
+}
+
+const schemaAF_Packet = {
+    type: 'object',
+    properties: {
+        Interface_Name: { type: 'string' },
+        Ethernet_Protocol_Number: { type: 'number' },
+        Packet_Type: { type: 'string' },
+        ARP_Hardware_Address_Type: { type: 'number' },
+        Hardware_Physical_Address: { type: 'string' }
+    },
+    required: ['Interface_Name', 'Ethernet_Protocol_Number', 'Packet_Type', 'ARP_Hardware_Address_Type'],
+    additionalProperties: false
+}
+
+const schemaInfo = {
+    type: 'object',
+    properties: {
+        Sniffed_Timestamp: { type: 'number' },
+        Processed_Timestamp: { type: 'number' },
+        Submitter_Timestamp: { type: 'number' },
+        Size: { type: 'number' },
+    },
+    required: ['Sniffed_Timestamp', 'Processed_Timestamp', 'Submitter_Timestamp', 'Size'],
+    additionalProperties: false
+}
+
+
+const schemaPacket_802_2 = {
+    type: 'object',
+    properties: {
+        DSAP: { type: 'string' },
+        SSAP: { type: 'string' },
+        Control: { type: 'string' },
+    },
+    required: ['DSAP', 'SSAP', 'Control'],
+    additionalProperties: false,
+}
+
+const schemaPacket_802_3 = {
+    type: 'object',
+    properties: {
+        Destination_MAC: { type: 'string' },
+        Source_MAC: { type: 'string' },
+        Ethertype: { type: 'number' },
+    }
+}
+
+const schemaUnknown = {
+    type: 'object',
+    properties: {
+        Message: { type: 'string' },
+        Protocol_Identifier: { type: 'number' }
+    }
+}
+
+
+const validateUnknown = ajv.compile(schemaUnknown);
+const validatePacket_802_3 = ajv.compile(schemaPacket_802_3);
+const validatePacket_802_2 = ajv.compile(schemaPacket_802_2);
+const validateAF_Packet = ajv.compile(schemaAF_Packet);
+const validateInfo = ajv.compile(schemaInfo);
+
+// Ajv compiles schemas to functions and caches them in all cases
+const validatePacket = ajv.compile(schemaPacket);
 
 // absloute path are use process.cwd and Path.join
 
@@ -41,7 +121,8 @@ function validSampleFiles(directory) {
 }
 
 
-function parsePacket(sample) {
+function parseToObject(sample) {
+    // parses data recursively, parses values as well
     try {
         return JSON.parse(sample);
     }
@@ -53,7 +134,25 @@ function parsePacket(sample) {
 
 function dynamicValidator(sampleObject) {
     // array of key 
-    Object.entries(sampleObject)
+    //const packet = Object.entries(sampleObject);
+
+    //console.log(typeof sampleObject['AF_Packet']);
+    if (validatePacket(sampleObject)) {
+        // top level valid
+        const protocols = Object.keys(sampleObject)
+
+        // validate all keys
+        console.log(validateInfo(sampleObject.Info))
+        console.log(validateAF_Packet(sampleObject.AF_Packet))
+    }
+
+    return false;
+
+    // check if key is a valid protocol
+    // check if key required AF_Packet and Info key is present
+    // validate the values which is also a Object
+
+    // only valid if all key , values are valid , and boolean values, if false not valid
 }
 
 function readValidSamples(sampleFile) {
@@ -63,11 +162,11 @@ function readValidSamples(sampleFile) {
 
         // perform conversion  using callback per samples
         samples.slice(0, 2).forEach(sample => {
-            const res = parsePacket(sample)
+            const res = parseToObject(sample)
 
             if (res) {
-                //dynamicValidator(res);
-                console.log(Object.entries(res));
+                dynamicValidator(res);
+                //console.log(Object.entries(res));
                 //console.log(res.__proto__)
                 //console.log(Object.getOwnPropertyNames(res))
             }
