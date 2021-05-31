b11c8e139cfb1bfb7d8b387240afa3453d983103
brennan <brennanhfredericks@gmail.com>
2021-05-31 12:30:49 +0200

    able to load files and lines asychronously

--- supporting_scripts/api_test_samples.js
+++ supporting_scripts/api_test_samples.js
@@ -1,5 +1,7 @@
-const { readdir, readFile, readLine } = require('fs');
+const { once } = require('events');
+const { readdir, readFile, readLine, createReadStream } = require('fs');
 const Path = require('path');
+const { createInterface } = require('readline');
 
 function Failure(msg) {
     //console.error(`Failure Callback: ${msg}`);
@@ -16,25 +18,75 @@ async function getFiles(directory) {
                 return;
             }
 
-            resolve(files);
+            // create file path
+            resolve(files.map((value) => {
+                return Path.join(directory, value);
+            }));
+
         })
     });
 }
 
+async function parseToObject(sample_string) {
+    try {
+
+        return JSON.parse(sample_string)
+    }
+    catch (err) {
+        console.error(`${err}`);
+        return null;
+    }
+}
+
+async function _loadSample(validFile) {
+    // avoid load large files into memory before processing 
+
+    try {
+        const rl = createInterface({
+            input: createReadStream(validFile),
+            crlfDelay: Infinity,
+        });
+
+        // register for on line event
+        rl.on('line', async (sample) => {
+            const packet = await parseToObject(sample);
+
+            if (packet) {
+                // able to parse string to JSON
+                console.info(packet);
+                // validate contents
+                //const valid 
+            }
+
+
+        })
+
+        // wait until we receive the file close event
+        await once(rl, 'close');
+    }
+    catch (err) {
+        console.error(`${err}`);
+    }
+}
+
 async function loadSamples(validFiles) {
+    // some files or samples in files could be invalid JSON,
+    // allow application to continue to the next line
 
-    console.log(validFiles);
-    // return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+        validFiles.forEach(_loadSample)
+        resolve('done loading samples');
+    });
 
-    // });
 }
 
 async function getValidSamples(files) {
     // could fail no vaild sample files
     return new Promise((resolve, reject) => {
         const validFiles = files.filter((value) => value.indexOf('lsp') >= 0);
+
         if (validFiles.length == 0) {
-            reject('no valid sample files')
+            reject('No valid sample files in the directory provided')
             return;
         }
 
@@ -47,6 +99,7 @@ const filesPromise = getFiles('./supporting_scripts/packet_sample_data/');
 
 filesPromise
     .then(getValidSamples, Failure)
+    .then(getValidSamples, Failure)
     .then(loadSamples, Failure)
-    // handle throw part
+    // handle error raised by Failure 
     .catch((reason) => { console.log(reason) });
