2e08aea869868352448d3a3cfafb63dea84ecd0b
brennan <brennanhfredericks@gmail.com>
2021-05-31 21:45:29 +0200

    added top level packer validator

--- fastify_api/routes/packets/packets.js
+++ fastify_api/routes/packets/packets.js
@@ -1,4 +1,17 @@
+/*
+*/
 
+async function packetPostHandler(request, reply) {
+    // validate internal data
+    console.log('here');
+    return { message: 'We 201' };
+}
+
+async function packetGetHandler(request, reply) {
+    console.log('here');
+
+    return { message: 'We 200' };
+}
 // add auto completion support
 /**
  * 
@@ -7,14 +20,31 @@
  */
 module.exports = async function (fastify, opts) {
 
-    // add schema for response
-    fastify.get('/', async function (request, reply) {
-        return { message: 'We 200' };
-    });
 
-    // add top level schema for post and dynamic validate
-    // add response schema
-    fastify.post('/', async function (request, reply) {
-        // add packets to endpoint
-    });
+    // add schema for response
+    fastify.get('/', packetGetHandler);
+
+    const bodyJsonSchema = {
+        type: 'object',
+        required: ['Info', 'AF_Packet'],
+        properties: {
+            Info: { type: 'object' },
+            AF_Packet: { type: 'object' }
+        },
+        additionalProperties: true
+    }
+
+    const headersJsonSchema = {
+        type: 'object',
+        properties: {
+            'content-type': { type: 'string' }
+        },
+        required: ['content-type']
+    }
+
+    const schema = {
+        body: bodyJsonSchema,
+        headers: headersJsonSchema
+    }
+    fastify.post('/', { schema }, packetPostHandler);
 }
